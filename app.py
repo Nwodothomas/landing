932d114f85fea367d073ade951299306cfe53368
Nwodothomas <108361891+Nwodothomas@users.noreply.github.com>
2023-06-17 00:03:39 +0100

    added files

--- app.py
+++ app.py
@@ -6,17 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-'''@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-'''
-
-@app.route('/docs/doc.html')
+@app.route('/docs/')
 def view_documentation():
     return render_template('docs/doc.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
