ec89522c09114b94e9968d63d868835307f8edb7
Nwodothomas <108361891+Nwodothomas@users.noreply.github.com>
2023-07-17 16:27:33 +0100

    refactor codes to make it responsive

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/docs/')
+@app.route('/doc/')
 def view_documentation():
-    return render_template('docs/doc.html')
+    return render_template('doc/doc.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
